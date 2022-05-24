20d8e4854145877d22965fa72613dac00e0552ec
Андрей Крылов <numinga27@yandex.ru>
2022-05-24 21:21:05 +0400

    gecn

---
+++ yatube/posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
+
+def group_posts(request, slug):
+    return HttpResponse(f'Страницы сообществ {slug}')
---
+++ yatube/templates/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('group_list/', views.group_list),
+    path('group/<slug:slug>/', views.group_posts)
+] 
